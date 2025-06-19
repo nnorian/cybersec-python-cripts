1de7b45335153ecf0c49d1e332f6beafb1769601
norian <160144830+nnorian@users.noreply.github.com>
2025-06-19 18:14:13 +0300

    lest update on python scripts for first week

--- crypto-tools/hash_cracker.py
+++ crypto-tools/hash_cracker.py
@@ -1,6 +1,14 @@
 # specifically made for my try hack me task 
 
 import hashlib
-import pyfiglet 
 
 wordlist_location = str(input("enter wordlist file location:"))
+hash_input = str(input("enter hash to be cracked: "))
+
+with open(wordlist_location, "r") as file:
+    for line in file.readlines():
+        hash_ob = hashlib.md5(line.strip().encode())
+        hased_pass = hash_ob.hexidigest()
+        if hashed_pass == hash_input:
+            print("found cleartext password!" + line.strip())
+            exit(0)
